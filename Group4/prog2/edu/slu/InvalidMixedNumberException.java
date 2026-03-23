ca03e58c77bfd90b1ba50c9a3fc94ef956ef1d14
BrianVelasco-blip <bpev2264040@slu.edu.ph>
2026-03-23 10:53:39 +0800

    Add files via upload

---
+++ Group4/prog2/edu/slu/InvalidMixedNumberException.java
@@ -0,0 +1,7 @@
+package prog2.edu.slu;
+
+public class InvalidMixedNumberException extends Exception {
+    public InvalidMixedNumberException(String message) {
+        super(message);
+    }
+}
---
+++ Group4/prog2/edu/slu/MainApp.java
@@ -0,0 +1,14 @@
+package prog2.edu.slu;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Entry point of the application
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        new ScientificCalculator();
+    }
+}
+
